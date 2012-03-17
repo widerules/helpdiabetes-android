75c77e3b18cbce354af77f5c699596b2aa7e425c
hippoandfriends@gmail.com <hippoandfriends@gmail.com@33dc98c9-0ed4-31b0-123a-82660d6943c9>
2012-03-17 08:24:07 +0000

    version received from Kenny, 16/03/2012

--- trunk/src/be/goossens/oracle/DbAdapter.java
+++ trunk/src/be/goossens/oracle/DbAdapter.java
@@ -1,36 +1,41 @@
 package be.goossens.oracle;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /*
  * This class provides the add, delete, create of all the database components
  */
 
-public class DbAdapter {
-	// create database
-	private static final String DATABASE_NAME = "db";
-	private static final int DATABASE_VERSION = 37;
-	private DatabaseHelper mDbHelper;
-	private SQLiteDatabase mDb;
-	private final Context mCtx;
+public class DbAdapter extends SQLiteOpenHelper {
+	// The android default system path to my application database
+		private static String DB_PATH = "/data/data/be.goossens.oracle/databases/";
+		private static String DB_NAME = "dbhelpdiabetesnl";
+		private static final int DATABASE_VERSION = 1;
+		private SQLiteDatabase mDb;
+		private final Context mCtx;
 
-	//settings
+	// settings
 	private static final String DATABASE_SETTINGS_TABLE = "settings";
-	public static final String DATBASE_SETTINGS_ID = "_id";
-	public static final String DATBASE_SETTINGS_USERID = "userid";
-	public static final String DATBASE_SETTINGS_INSULINE_RATIO_BREAKFAST = "insulinebreakfast";
-	public static final String DATBASE_SETTINGS_INSULINE_RATIO_LUNCH = "insulinelunch";
-	public static final String DATBASE_SETTINGS_INSULINE_RATIO_SNACK = "insulinesnack";
-	public static final String DATBASE_SETTINGS_INSULINE_RATIO_DINNER = "insulinedinner";
-	public static final String DATBASE_SETTINGS_MEAL_TIME_BREAKFAST = "mealtimebreakfast";
-	public static final String DATBASE_SETTINGS_MEAL_TIME_LUNCH = "mealtimelunch";
-	public static final String DATBASE_SETTINGS_MEAL_TIME_SNACK = "mealtimesnack";
-	public static final String DATBASE_SETTINGS_MEAL_TIME_DINNER = "mealtimedinner";
+	public static final String DATABASE_SETTINGS_ID = "_id";
+	public static final String DATABASE_SETTINGS_INSULINE_RATIO_BREAKFAST = "insulinebreakfast";
+	public static final String DATABASE_SETTINGS_INSULINE_RATIO_LUNCH = "insulinelunch";
+	public static final String DATABASE_SETTINGS_INSULINE_RATIO_SNACK = "insulinesnack";
+	public static final String DATABASE_SETTINGS_INSULINE_RATIO_DINNER = "insulinedinner";
+	public static final String DATABASE_SETTINGS_MEAL_TIME_BREAKFAST = "mealtimebreakfast";
+	public static final String DATABASE_SETTINGS_MEAL_TIME_LUNCH = "mealtimelunch";
+	public static final String DATABASE_SETTINGS_MEAL_TIME_SNACK = "mealtimesnack";
+	public static final String DATABASE_SETTINGS_MEAL_TIME_DINNER = "mealtimedinner";
 	
 	// SelectedFood
 	private static final String DATABASE_SELECTEDFOOD_TABLE = "selectedfood";
@@ -46,20 +51,6 @@ public class DbAdapter {
 	public static final String DATABASE_SELECTEDFOOD_FOODID = "foodid";
 	public static final String DATABASE_SELECTEDFOOD_UNITID = "unitid";
 
-	private static final String DATABASE_SELECTEDFOOD_CREATE = "create table "
-			+ DATABASE_SELECTEDFOOD_TABLE + " (" + DATABASE_SELECTEDFOOD_ID
-			+ " integer primary key autoincrement, "
-			+ DATABASE_SELECTEDFOOD_AMOUNT + " float not null, "
-			+ DATABASE_SELECTEDFOOD_FOODNAME + " text not null, "
-			+ DATABASE_SELECTEDFOOD_UNITNAME + " text not null, "
-			+ DATABASE_SELECTEDFOOD_KCAL + " float nog null, "
-			+ DATABASE_SELECTEDFOOD_PROTEIN + " float not null, "
-			+ DATABASE_SELECTEDFOOD_CARBS + " float not null, "
-			+ DATABASE_SELECTEDFOOD_FAT + " float not null, "
-			+ DATABASE_SELECTEDFOOD_STANDARDAMOUNT + " float not null, "
-			+ DATABASE_SELECTEDFOOD_FOODID + " float not null, "
-			+ DATABASE_SELECTEDFOOD_UNITID + " float not null);";
-
 	// Food
 	public static final String DATABASE_FOOD_TABLE = "food";
 	public static final String DATABASE_FOOD_ID = "_id";
@@ -70,16 +61,6 @@ public class DbAdapter {
 	public static final String DATABASE_FOOD_VISIBLE = "visible";
 	public static final String DATABASE_FOOD_FOODLANGUAGEID = "foodlanguageid";
 	public static final String DATABASE_FOOD_PLATFORM = "platform";
-	private static final String DATABASE_FOOD_CREATE = "create table "
-			+ DATABASE_FOOD_TABLE + " (" + DATABASE_FOOD_ID
-			+ " integer primary key autoincrement, " + DATABASE_FOOD_NAME
-			+ " text not null, " + DATABASE_FOOD_ISFAVORITE
-			+ " integer not null, " + DATABASE_FOOD_USERID
-			+ " integer not null, " + DATABASE_FOOD_CATEGORYID
-			+ " integer not null, " + DATABASE_FOOD_VISIBLE
-			+ " integer not null, " + DATABASE_FOOD_FOODLANGUAGEID
-			+ " integer not null, " + DATABASE_FOOD_PLATFORM
-			+ " text not null);";
 
 	// FOODUNIT
 	public static final String DATABASE_FOODUNIT_TABLE = "foodunit";
@@ -93,57 +74,133 @@ public class DbAdapter {
 	public static final String DATABASE_FOODUNIT_FAT = "fat";
 	public static final String DATABASE_FOODUNIT_VISIBLE = "visible";
 	public static final String DATABASE_FOODUNIT_FOODID = "foodid";
-	private static final String DATABASE_FOODUNIT_CREATE = "create table "
-			+ DATABASE_FOODUNIT_TABLE + " (" + DATABASE_FOODUNIT_ID
-			+ " integer primary key autoincrement, " + DATABASE_FOODUNIT_FOODID
-			+ " integer not null, " + DATABASE_FOODUNIT_NAME
-			+ " text not null, " + DATABASE_FOODUNIT_DESCRIPTION
-			+ " text null, " + DATABASE_FOODUNIT_STANDARDAMOUNT
-			+ " float not null, " + DATABASE_FOODUNIT_KCAL
-			+ " float not null, " + DATABASE_FOODUNIT_PROTEIN
-			+ " float not null, " + DATABASE_FOODUNIT_CARBS
-			+ " float not null, " + DATABASE_FOODUNIT_FAT + " float not null, "
-			+ DATABASE_FOODUNIT_VISIBLE + " integer not null);";
-
-	private static class DatabaseHelper extends SQLiteOpenHelper {
-		DatabaseHelper(Context ctx) {
-			super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
-		}
-
-		// this will create the database tables
-		@Override
-		public void onCreate(SQLiteDatabase db) {
-			db.execSQL(DATABASE_FOOD_CREATE);
-			db.execSQL(DATABASE_FOODUNIT_CREATE);
-			db.execSQL(DATABASE_SELECTEDFOOD_CREATE);
-		}
-
-		// this will destroy the data in the database and recreate the tables
-		@Override
-		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-			db.execSQL("DROP TABLE IF EXISTS " + DATABASE_FOOD_TABLE);
-			db.execSQL("DROP TABLE IF EXISTS " + DATABASE_FOODUNIT_TABLE);
-			db.execSQL("DROP TABLE IF EXISTS " + DATABASE_SELECTEDFOOD_TABLE);
-			onCreate(db);
-		}
-	}
-
+	
+	
 	public DbAdapter(Context ctx) {
+		super(ctx,DB_NAME,null,DATABASE_VERSION);
 		this.mCtx = ctx;
 	}
 
-	public DbAdapter open() throws SQLException {
-		mDbHelper = new DatabaseHelper(mCtx);
-		mDb = mDbHelper.getWritableDatabase();
-		return this;
+	/*
+	 * Create a empty database on the system and rewrites it with the
+	 * databasefile
+	 */
+	public void createDatabase() {
+		if (!checkDatabase()) {
+			/*
+			 * This method will create a empty database
+			 */
+			this.close();
+			this.getReadableDatabase();
+			copyDatabase();
+		}
 	}
 
-	public void close() {
-		mDbHelper.close();
+	// Copy your database from the local asset folder to the just created empty
+	// database
+	private void copyDatabase() {
+		try {
+			// Open the local db as the input stream
+			InputStream myInput = mCtx.getAssets().open(DB_NAME);
+			// Path to the just created empty db
+			String outFileName = DB_PATH + DB_NAME;
+			// Open the empty db as the output stream
+			OutputStream myOutput = new FileOutputStream(outFileName);
+			// transfer bytes from the inputfile to the outputfile
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = myInput.read(buffer)) > 0) {
+				myOutput.write(buffer, 0, length);
+			}
+			// close the streams
+			myOutput.flush();
+			myOutput.close();
+			myInput.close();
+
+		} catch (IOException e) {
+		}
 	}
 
-	public void createData(String query) {
-		mDb.execSQL(query);
+	@Override
+	public synchronized void close() {
+		if (mDb != null) {
+			mDb.close();
+		}
+		super.close();
+	}
+
+	// Check if the database already exists
+	public boolean checkDatabase() {
+		SQLiteDatabase checkDB = null;
+		try {
+			String myPath = DB_PATH + DB_NAME;
+			checkDB = SQLiteDatabase.openDatabase(myPath, null,
+					SQLiteDatabase.OPEN_READONLY);
+		} catch (SQLiteException e) {
+			// The database does not exists
+		}
+		return checkDB != null ? true : false;
+		//return false;
+	}
+
+	public void open() throws SQLException{
+		String myPath = DB_PATH + DB_NAME;
+		mDb = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+	}
+	
+	// SETTINGS Functions
+	// create settings
+	public long createSettings() {
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(DATABASE_SETTINGS_INSULINE_RATIO_BREAKFAST, "0");
+		initialValues.put(DATABASE_SETTINGS_INSULINE_RATIO_LUNCH, "0");
+		initialValues.put(DATABASE_SETTINGS_INSULINE_RATIO_SNACK, "0");
+		initialValues.put(DATABASE_SETTINGS_INSULINE_RATIO_DINNER, "0");
+		initialValues.put(DATABASE_SETTINGS_MEAL_TIME_BREAKFAST, "06:00");
+		initialValues.put(DATABASE_SETTINGS_MEAL_TIME_LUNCH, "12:00");
+		initialValues.put(DATABASE_SETTINGS_MEAL_TIME_SNACK, "16:00");
+		initialValues.put(DATABASE_SETTINGS_MEAL_TIME_DINNER, "18:00");
+		return mDb.insert(DATABASE_SETTINGS_TABLE, null, initialValues);
+	}
+
+	// get all settings
+	public Cursor fetchAllSettings() {
+		return mDb.query(DATABASE_SETTINGS_TABLE, new String[] {
+				DATABASE_SETTINGS_ID,
+				DATABASE_SETTINGS_INSULINE_RATIO_BREAKFAST,
+				DATABASE_SETTINGS_INSULINE_RATIO_LUNCH,
+				DATABASE_SETTINGS_INSULINE_RATIO_SNACK,
+				DATABASE_SETTINGS_INSULINE_RATIO_DINNER,
+				DATABASE_SETTINGS_MEAL_TIME_BREAKFAST,
+				DATABASE_SETTINGS_MEAL_TIME_LUNCH,
+				DATABASE_SETTINGS_MEAL_TIME_SNACK,
+				DATABASE_SETTINGS_MEAL_TIME_DINNER }, null, null, null, null,
+				null);
+	}
+
+	// Update settings insuline ratio
+	public boolean updateSettingsInsulineRatio(long id, float breakfastRatio,
+			float lunchRatio, float snackRatio, float dinnerRatio) {
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(DATABASE_SETTINGS_INSULINE_RATIO_BREAKFAST,
+				breakfastRatio);
+		initialValues.put(DATABASE_SETTINGS_INSULINE_RATIO_LUNCH, lunchRatio);
+		initialValues.put(DATABASE_SETTINGS_INSULINE_RATIO_SNACK, snackRatio);
+		initialValues.put(DATABASE_SETTINGS_INSULINE_RATIO_DINNER, dinnerRatio);
+		return mDb.update(DATABASE_SETTINGS_TABLE, initialValues,
+				DATABASE_SETTINGS_ID + "=" + id, null) > 0;
+	}
+
+	// Update settings meal times
+	public boolean updateSettingsMealTimes(long id, String breakfastTime,
+			String lunchTime, String snackTime, String dinnerTime) {
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(DATABASE_SETTINGS_MEAL_TIME_BREAKFAST, breakfastTime);
+		initialValues.put(DATABASE_SETTINGS_MEAL_TIME_LUNCH, lunchTime);
+		initialValues.put(DATABASE_SETTINGS_MEAL_TIME_SNACK, snackTime);
+		initialValues.put(DATABASE_SETTINGS_MEAL_TIME_DINNER, dinnerTime);
+		return mDb.update(DATABASE_SETTINGS_TABLE, initialValues,
+				DATABASE_SETTINGS_ID + "=" + id, null) > 0;
 	}
 
 	// SELECTED FOOD Functions
@@ -330,7 +387,7 @@ public class DbAdapter {
 		ContentValues initialValues = new ContentValues();
 		initialValues.put(DATABASE_FOODUNIT_FOODID, foodId);
 		initialValues.put(DATABASE_FOODUNIT_NAME, name);
-		//initialValues.put(DATABASE_FOODUNIT_DESCRIPTION, " ");
+		// initialValues.put(DATABASE_FOODUNIT_DESCRIPTION, " ");
 		initialValues.put(DATABASE_FOODUNIT_STANDARDAMOUNT, standardAmount);
 		initialValues.put(DATABASE_FOODUNIT_KCAL, kcal);
 		initialValues.put(DATABASE_FOODUNIT_PROTEIN, protein);
@@ -403,5 +460,17 @@ public class DbAdapter {
 		initialValues.put(DATABASE_FOODUNIT_FAT, fat);
 		return mDb.update(DATABASE_FOODUNIT_TABLE, initialValues,
 				DATABASE_FOODUNIT_ID + "=" + unitId, null) > 0;
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
 	}
 }
